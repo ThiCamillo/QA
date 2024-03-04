6fb2a1d6f22ae9ea6731aa6dd12b80a0b643c0ff
Thiago Camillo <thiago.sam.camillo@gmail.com>
2024-03-04 17:13:44 -0300

    first commit

---
+++ Bootcamp QAzando/array.js
@@ -0,0 +1,4 @@
+const paises = ['Brasil', 'Argentina', 'Alemanha'] //array - lista
+
+
+console.log(paises[3])
---
+++ Bootcamp QAzando/variaveis.js
@@ -0,0 +1,28 @@
+// pode ter o seu valor alterado!
+var nome = 'Thiago' //string - boolean - 
+nome = 'Pedro'
+console.log(nome)
+
+//pode ter seu valor alterado
+let = idade = 20 // numeros
+idade = 30
+console.log(idade)
+
+// não pode ter seu valor alterado
+const cidade = 'Florianopolis'
+//cidade = 'Sao Paulo' // da erro pois não pode ser alterado
+
+const paises = ['Brasil', 'Argentina', 'Alemanha'] //array - lista
+
+//objeto - chave e valor
+let devices = {
+    nome: 'iPhone 14',
+    preco: 8000,
+    cores: {
+        cor1:'vermelho',
+        cor2: 'azul'
+    }
+}
+
+//função
+
