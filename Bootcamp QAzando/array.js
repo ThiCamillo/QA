e6fe47654c6ce8ada96cc76f08719f3181b1db3b
Thiago Camillo <thiago.sam.camillo@gmail.com>
2024-03-06 18:04:50 -0300

    decisao

--- Bootcamp QAzando/array.js
+++ Bootcamp QAzando/array.js
@@ -1,4 +1,13 @@
-const paises = ['Brasil', 'Argentina', 'Alemanha'] //array - lista
+const paises = ['Brasil', 'Argentina', 'Alemanha'] //array - lista de dados
 
+console.log('Acessando o país: ' + paises[0]) //acessando uma posição do array
+console.log('Tamanho do array: ' + paises.length) //tamanho do array
 
-console.log(paises[3])
+//operações com o array
+
+paises.push('USA') //adiciona no final do array
+paises.pop() //remove ultimo elemento
+paises.unshift('França') // adiciona no começo
+paises.shift() //remove primeiro elemento
+
+console.log(paises)
---
+++ Bootcamp QAzando/decisao.js
@@ -0,0 +1,22 @@
+// if-else
+// if (condição) {
+// } else {
+//}
+
+const nome  = 'Eduardo'
+
+/* if (nome == 'thiago') {
+    console.log('Condição atendida')
+} else {
+    console.log('Condição não atendida')
+} */
+
+let idade = 10
+
+if (idade == 30 ){
+    console.log('idade 30')
+} else if (idade == 20) {
+    console.log('idade 20')
+} else {
+    console.log('Nenhuma condição atendida')
+}
