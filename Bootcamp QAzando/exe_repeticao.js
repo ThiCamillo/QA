1bcb383370844bfce8fc43ccbd078032ed3e04b5
Thiago Camillo <thiago.sam.camillo@gmail.com>
2024-03-27 18:13:32 -0300

    resposta exercicio

--- Bootcamp QAzando/exe_repeticao.js
+++ Bootcamp QAzando/exe_repeticao.js
@@ -3,9 +3,27 @@
 //percorrer uma lista de nomes, contendo os nomes:
 //Eduardo, Maria, Fernando, Joao e Francisco
 //numero da execução, começando em 1
-//nome que esrta sendo executado
+//nome que esta sendo executado
 //separadores
 
 //Execução: 1
 //Nome: Eduardo
 //-------------
+
+const nomes = ['Eduardo', 'Maria', 'Fernando', 'Joao', 'Francisco']
+
+//nomes.forEach((pessoa, indice) => {
+ //   console.log('Execução: ' + indice)
+ //   console.log(pessoa)
+//})
+
+//cidades.forEach((elemento, index) => {
+//console.log('Execução numero: ' + index)
+//console.log(elemento)
+//})
+
+for (let indice = 1; indice < nomes.length; indice++) {
+    console.log('Execução: ' + indice)
+    console.log('Nome: ' + nomes[indice])
+    console.log('-----------------')
+}
