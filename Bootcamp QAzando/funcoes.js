2f6926a4b90384ac5fa8bc19de4565e4f199602a
Thiago Camillo <thiago.sam.camillo@gmail.com>
2024-03-28 15:34:58 -0300

    return

--- Bootcamp QAzando/funcoes.js
+++ Bootcamp QAzando/funcoes.js
@@ -1,12 +1,18 @@
 //FUNÇÕES = bloco de codigo que a gente chama para ser executado - metodo
 // pode ou nao receber paramentros
 //pode ou nao retornar dados
-
+/*
 function gerarNome(nome, sobrenome, idade) {
     console.log('Nome:', nome, sobrenome)
     console.log('Idade:', idade)
-
 }
+*/
+//colocando retorno na função
 
-gerarNome('Luiz', 'da Silva', 18)
-gerarNome('Luiz', 'da Silva', 18)
+function gerarNome(nome, sobrenome) {
+    return ('Nome: ' + nome + sobrenome)
+}
+//console.log(gerarNome('Luiz', ' da Silva'))
+
+const nomeGerado = gerarNome('Pedro', ' Carvalho')
+console.log(nomeGerado)
