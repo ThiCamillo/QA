b483d6d127a2a57673a1e08acc56347810c91ecf
Thiago Camillo <thiago.sam.camillo@gmail.com>
2024-03-28 12:09:43 -0300

    funcao

---
+++ Bootcamp QAzando/funcoes.js
@@ -0,0 +1,15 @@
+//FUNÇÕES = bloco de codigo que a gente chama para ser executado - metodo
+// pode ou nao receber paramentros
+//pode ou nao retornar dados
+
+function gerarNome() {
+    const nome = 'Thiago'
+    const sobrenome = 'Sampaio'
+    const idade = 38
+
+    console.log('Nome:', nome, sobrenome)
+    console.log('Idade:', idade)
+
+}
+
+gerarNome()
